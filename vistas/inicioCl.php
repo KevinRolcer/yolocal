d885a6ca69c349a96fade8ade149f28cb5edabf2
Rolcer <kevinrolce6@gmail.com>
2025-09-10 14:35:37 -0600

    s

--- vistas/inicioCl.php
+++ vistas/inicioCl.php
@@ -355,7 +355,7 @@
                             Explora mas tipos de negocios que forman parte de Yolocal
                         </div>
                         <div class="card-image">
-                            <img src="../assets/img/LogoYolocal.png" alt="">
+                            <img src="assets/img/LogoYolocal.png" alt="">
                         </div>
                     </div>
                     <div class="card-footer">
