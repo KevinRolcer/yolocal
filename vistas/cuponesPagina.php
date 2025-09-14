103660caac44f4550cdbb2cf09edefb37e39030c
ArathCode <l21240065@smartin.tecnm.mx>
2025-09-14 11:50:04 -0600

    Cupones en pagina listos

--- vistas/cuponesPagina.php
+++ vistas/cuponesPagina.php
@@ -37,7 +37,7 @@
                     </svg>
                     <span class="texto-menu">Inicio</span>
                 </a>
-               <a href="../controladores/NegocioLControlador.php" class="enlace" data-tooltip="Inicio">
+                <a href="../controladores/NegocioLControlador.php" class="enlace" data-tooltip="Inicio">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z" />
                     </svg>
@@ -88,10 +88,10 @@
     <div class="principal">
 
         <div class="filter-container">
-            
 
 
-             <div class="search-bar " data-filter="descripcion">
+
+            <div class="search-bar " data-filter="descripcion">
                 <input type="text" id="filtroDescripcion" placeholder="Buscar descripción" />
                 <button type="button">
                     <i class="bi bi-search"></i>
@@ -107,7 +107,11 @@
 
         <div id="paginacion" class="mt-3 d-flex justify-content-center"></div>
         <div class="descripcionC">
-            <a href="#">Ver más cupones</a>
+
+            <p>Sumérgete en el mundo de los cupones y descuentos exclusivos para
+                <span>Yo <span id="span2">local</span></span>
+            </p>
+
         </div>
 
     </div>
