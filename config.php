e3f35f053200b493da60958b13eb20d139e0145b
Rolcer <kevinrolce6@gmail.com>
2025-09-28 15:28:43 -0600

    s

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
     {
         static $conexion;
 
-        if(!isset($conexion)) 
+        if(!isset($connection)) 
         {
             $config = parse_ini_file('config.ini'); 
             $conexion = mysqli_connect($config['servidor'],$config['usuario'],$config['pass'],$config['bbdd']);
