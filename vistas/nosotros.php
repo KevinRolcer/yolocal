72b731fa3ea24be3e090729f38c72a59f1945a1c
Rolcer <kevinrolce6@gmail.com>
2025-10-03 10:35:03 -0600

    Propuesta de diseño para nosotros

--- vistas/nosotros.php
+++ vistas/nosotros.php
@@ -163,52 +163,46 @@
 
         <section class="valores fade-in" id="valores">
             <h2 class="section-title">Nuestros Valores</h2>
-            <div class="valores-grid">
-                <div class="valor-card">
-                    <div class="valor-icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                        </svg>
-                    </div>
-                    <h3>Cercanía</h3>
-                    <p>Somos comunidad, vecinos y aliados. Juntos construimos un Texmelucan más fuerte.</p>
+
+            <div class="grid-container">
+                <div id="itemImg" class="grid-item">
+                    <img src="https://i.pinimg.com/736x/43/2c/d3/432cd36c93689f0ece8e244ef0b5ee29.jpg" alt="animales">
                 </div>
-                <div class="valor-card">
-                    <div class="valor-icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                        </svg>
+                
+                <div class="grid-item morado">
+                    <div>
+                        <h2>Orgullo Local</h2>
+                        <small>Promovemos lo nuestro, lo que hace grande a Texmelucan y nos llena de identidad.</small>
                     </div>
-                    <h3>Orgullo Local</h3>
-                    <p>Promovemos lo nuestro, lo que hace grande a Texmelucan y nos llena de identidad.</p>
                 </div>
-                <div class="valor-card">
-                    <div class="valor-icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
-                        </svg>
+                
+                <div id="itemImg" class="grid-item">
+                    <img src="https://i.pinimg.com/736x/43/2c/d3/432cd36c93689f0ece8e244ef0b5ee29.jpg" alt="molino">
+                </div>
+                
+                <div class="grid-item naranja">
+                    <span class="logo">Cercanía</span>
+                </div>
+                
+                <div class="grid-item amarillo">
+                    <span class="logo">Colaboración</span>
+                </div>
+                
+                <div id="itemImg" class="grid-item">
+                    <img src="https://i.pinimg.com/736x/43/2c/d3/432cd36c93689f0ece8e244ef0b5ee29.jpg" alt="campo">
+                </div>
+                
+                <div class="grid-item complementario">
+                    <div>
+                        <h2>Compromiso</h2>
+                        <small>Trabajamos de corazón para que lo local tenga voz, visibilidad y fuerza.</small>
                     </div>
-                    <h3>Empoderamiento</h3>
-                    <p>Compartimos herramientas, consejos y apoyo para que cada emprendedor dé su siguiente paso.</p>
                 </div>
-                <div class="valor-card">
-                    <div class="valor-icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
-                        </svg>
-                    </div>
-                    <h3>Colaboración</h3>
-                    <p>Juntos logramos más, apoyando y difundiendo los esfuerzos de cada quien.</p>
+                
+                <div id="itemImg" class="grid-item">
+                    <img src="../assets/img/LogoYolocal.png" alt="vaca">
                 </div>
-                <div class="valor-card">
-                    <div class="valor-icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
-                        </svg>
-                    </div>
-                    <h3>Compromiso</h3>
-                    <p>Trabajamos de corazón para que lo local tenga voz, visibilidad y fuerza.</p>
-                </div>
+
             </div>
         </section>
     </div>
