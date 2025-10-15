c07a3910974b33bc49bbb86676169a39f0dedfe7
ArathCode <l21240065@smartin.tecnm.mx>
2025-10-14 18:28:50 -0600

    encabezao

--- vistas/cuponesPagina.php
+++ vistas/cuponesPagina.php
@@ -3,7 +3,7 @@
 
 <head>
     <?php
-    include_once("SistemaAdmin/head.php");
+    include_once("sistemaAdmin/head.php");
     ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
